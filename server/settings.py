bc7f80e1066a7ff384e9ef43cfb069f774f14dd8
鲁浩 <416005376@qq.com>
2016-02-15 18:26:58 +0800

    change port.

--- server/settings.py
+++ server/settings.py
@@ -3,7 +3,7 @@
 import os.path
 from tornado.options import define
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=8001, help="run on the given port", type=int)
 define("config", default=None, help="tornado config file")
 define("debug", default=False, help="debug mode")
 
